39293971f145670050e100a665965343cd8a2ed8
alimomani <ali.almomany25@gmail.com>
2021-11-02 22:11:46 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class GridApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('grid app'),
+        leading: Icon(
+          Icons.menu,
+        ),
+      ),
+    );
+  }
+}
